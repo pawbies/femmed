2ec2611dc3817582b8eeab8e4a7dea9698b91b78
pawbies <alex.fuchs@pawbies.net>
2026-02-17 19:05:49 +0100

    register functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   has_many :user_medications
   has_many :medication_versions, through: :user_medications
 
+  validates :email_address, presence: true, uniqueness: true
+  validates :username, presence: true
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
