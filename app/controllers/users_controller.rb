72e9537aa6923fe2a9a7103ce9d82542a8311887
pawbies <alex.fuchs@pawbies.net>
2026-02-17 20:44:07 +0100

    user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   allow_unauthenticated_access only: %i[ new create ]
+  before_action :require_own_user, only: %i[ show edit update destroy ]
+  before_action :fetch_user_by_id, only: %i[ show edit update destroy ]
   layout "sessions", only: %i[ new create ]
 
   def new
@@ -17,8 +19,42 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_update_params
+      redirect_to @user, notice: "Updated #{@user.username} ^^"
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
+  def destroy
+    @user.destroy!
+
+    redirect_to landing_path, notice: "Sorry to see you go"
+  end
+
   private
     def user_params
       params.expect(user: [:email_address, :username, :password, :password_confirmation])
+    end
+
+    def user_update_params
+      params.expect(user: [:email_address, :username])
+    end
+
+    def require_own_user
+      if params[:id].to_i != Current.user.id
+        redirect_to root_path, alert: "Hey! You can't be there!"
+      end
+    end
+
+    def fetch_user_by_id
+      @user = User.find(params[:id])
     end
 end
