47b34ed6a9dc90065bc0f0edad9d967da7b55e80
pawbies <alex.fuchs@pawbies.net>
2026-02-17 14:30:37 +0100

    landing page, password and session forms

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,16 @@
 class SiteController < ApplicationController
+  allow_unauthenticated_access
+  before_action :redirect_to_landing_unless_authenticated, only: :index
+  layout "sessions", only: :landing
+
   def index
   end
+
+  def landing
+  end
+
+  private
+    def redirect_to_landing_unless_authenticated
+      redirect_to :landing unless authenticated?
+    end
 end
--- test/controllers/site_controller_test.rb
+++ test/controllers/site_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class SiteControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should get index unless unauthenticated" do
     get root_url
-    assert_response :success
+    assert_redirected_to landing_url
   end
 end
