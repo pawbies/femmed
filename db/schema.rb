a02dde8869a2fa0ba6e9cae126f369af3b1ef2cb
pawbies <alex.fuchs@pawbies.net>
2026-02-17 16:11:14 +0100

    categories model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_17_150654) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_17_150948) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_17_150654) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "references", force: :cascade do |t|
