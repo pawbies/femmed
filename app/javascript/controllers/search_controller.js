67975f46747c83fc1a600587c84bfc09f60019f4
pawbies <alex.fuchs@pawbies.net>
2026-02-20 17:24:11 +0100

    hidden search options

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="search"
+export default class extends Controller {
+  static targets = ["hiddenOptions"]
+
+  toggleHiddenOptions() {
+    this.hiddenOptionsTarget.classList.toggle("hidden");
+  }
+}
