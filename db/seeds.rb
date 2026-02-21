ab13838be649428cc6602dbf33b5f3119c06435a
pawbies <alex.fuchs@pawbies.net>
2026-02-21 11:33:14 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  {name: "Methylphenidathydrochlorid"}
+].each do |a|
+  ActiveIngredient.find_or_create_by(name: a[:name])
+end
+
+[
+  {name: "Novartis"}
+].each do |l|
+  Labeler.find_or_create_by(name: l[:name])
+end
+
+[
+  {name: "Capsules"}
+].each do |f|
+  Form.find_or_create_by(name: f[:name])
+end
+
+[
+  {
+    name: "Ritalin LA",
+    labeler: Labeler.find_by(name: "Novartis"),
+    form: Form.find_by(name: "Capsules")
+  },
+  {
+    name: "Ritalin IR",
+    labeler: Labeler.find_by(name: "Novartis"),
+    form: Form.find_by(name: "Capsules")
+  }
+].each do |m|
+  Medication.find_or_create_by(name: m[:name]) do |medication|
+    medication.labeler = m[:labeler]
+    medication.form = m[:form]
+  end
+end
+
