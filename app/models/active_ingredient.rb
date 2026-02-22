6a5835f64e66b3ec766e04ca44073458581d08ed
pawbies <alex.fuchs@pawbies.net>
2026-02-22 16:41:55 +0100

    better fixtures

--- app/models/active_ingredient.rb
+++ app/models/active_ingredient.rb
@@ -2,5 +2,5 @@ class ActiveIngredient < ApplicationRecord
   has_and_belongs_to_many :medications
 
   validates :name, presence: true, uniqueness: true
-  # validate half_life in the future
+  # validates half_life, numericality: { only_numeric: true }
 end
