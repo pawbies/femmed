42fc67ac20e078dbbd03f985727d1d11e67e72e9
pawbies <alex.fuchs@pawbies.net>
2026-02-22 20:09:37 +0100

    oh thats why

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
   after_create :create_default_settings!
 
   # Figure out encryption but ts breaks all the tests rn
-  # encrypts :email_address, deterministic: true
-  # encrypts :username
+  encrypts :email_address, deterministic: true
+  encrypts :username
 
   validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,6 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.active_record.encryption.encrypt_fixtures = true
 
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
