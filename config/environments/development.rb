a5001a01f0f8075278194bbbbc70e557893c6a12
pawbies <alex.fuchs@pawbies.net>
2026-02-23 18:03:53 +0100

    praying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # Using fixtures to test instead of seeds
   config.active_record.encryption.encrypt_fixtures = true
 
   # Make code changes take effect immediately without server restart.
