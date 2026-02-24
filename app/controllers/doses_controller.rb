a0e87822315848dce885ead7a42220cad0a28b8f
pawbies <alex.fuchs@pawbies.net>
2026-02-24 19:14:18 +0100

    stash check

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,7 @@
 class DosesController < ApplicationController
   before_action :fetch_prescription_by_id
   before_action :require_own_prescription
+  before_action :require_non_empty_stash, only: %i[ new create ]
 
   def new
     @dose = @prescription.doses.new(taken_at: Time.current.change(sec: 0))
@@ -23,6 +24,12 @@ class DosesController < ApplicationController
 
     def require_own_prescription
       redirect_to root_path, alert: "Grrrrr!" unless @prescription.user.id == Current.user.id
+    end
+
+    def require_non_empty_stash
+      unless @prescription.remaining_units > 0
+        redirect_back fallback_location: @prescription, notice: "You don't have anything to take cutie >.<"
+      end
     end
 
     def dose_params
