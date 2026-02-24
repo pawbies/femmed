1c72f32bf3dbefdd25e9e8c1eaf6868dde3c3a72
pawbies <alex.fuchs@pawbies.net>
2026-02-24 17:43:15 +0100

    small detail

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   belongs_to :prescription
 
-  validates :amount_taken, presence: true, numericality: true, comparison: { greater_than: 0, less_than_or_equal_to: ->(record) { record.prescription.remaining_units } }
+  validates :amount_taken, presence: true, numericality: true, comparison: { greater_than: 0, less_than_or_equal_to: ->(record) { record.prescription&.remaining_units } }
   validates :taken_at, presence: true, comparison: { less_than_or_equal_to: -> { Time.current } }
 end
