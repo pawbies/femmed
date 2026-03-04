1ee732974dd36a0ec5d4be0219cfe8b24994a099
pawbies <alex.fuchs@pawbies.net>
2026-03-04 11:38:11 +0100

    active ingredients tests and search fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,18 @@
 class SearchController < ApplicationController
   def index
-    @query = params[:query].strip
+    @query = params[:query]
 
-    @active_ingredients = ActiveIngredient.where("LOWER(name) LIKE LOWER(?)", "%#{@query}%").order(:name)
-    @categories         = Category.where("LOWER(name) LIKE LOWER(?)",         "%#{@query}%").order(:name)
-    @labelers           = Labeler.where("LOWER(name) LIKE LOWER(?)",          "%#{@query}%").order(:name)
-    @medications        = Medication.where("LOWER(name) LIKE LOWER(?)",       "%#{@query}%").order(:name)
+    if @query.present?
+      @query.strip!
+      @active_ingredients = ActiveIngredient.where("LOWER(name) LIKE LOWER(?)", "%#{@query}%").order(:name)
+      @categories         = Category.where("LOWER(name) LIKE LOWER(?)",         "%#{@query}%").order(:name)
+      @labelers           = Labeler.where("LOWER(name) LIKE LOWER(?)",          "%#{@query}%").order(:name)
+      @medications        = Medication.where("LOWER(name) LIKE LOWER(?)",       "%#{@query}%").order(:name)
+    else
+      @active_ingredients = ActiveIngredient.all
+      @categories         = Category.all
+      @labelers           = Labeler.all
+      @medications        = Medication.all
+    end
   end
 end
--- test/controllers/active_ingredients_controller_test.rb
+++ test/controllers/active_ingredients_controller_test.rb
@@ -2,8 +2,21 @@ require "test_helper"
 
 class ActiveIngredientsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @active_ingredient = active_ingredients(:amphetamine)
     @user  = users(:alice)
     @admin = users(:admin)
+  end
+
+  # show
+  test "should redirect show if not authenticated" do
+    get active_ingredient_path(@active_ingredient)
+    assert_redirected_to new_session_path
+  end
+
+  test "should get show if authenticated" do
+    sign_in_as(@user)
+    get active_ingredient_path(@active_ingredient)
+    assert_response :success
   end
 
   # new
@@ -32,17 +45,16 @@ class ActiveIngredientsControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect create if not admin" do
     sign_in_as(@user)
-    post active_ingredients_path, params: { active_ingredient: { name: "Test" } }
+    assert_no_difference "ActiveIngredient.count" do
+      post active_ingredients_path, params: { active_ingredient: { name: "Test" } }
+    end
     assert_redirected_to root_path
   end
 
   test "should create active ingredient with valid params" do
     sign_in_as(@admin)
-    assert_difference "ActiveIngredient.count", 1 do
-      post active_ingredients_path, params: { active_ingredient: {
-        name: "Methylphenidate :3",
-        half_life: 3.5
-      } }
+    assert_difference "ActiveIngredient.count" do
+      post active_ingredients_path, params: { active_ingredient: { name: "Methylphenidate :3", half_life: 3.5 } }
     end
     assert_redirected_to search_path(query: "Methylphenidate :3")
   end
@@ -50,11 +62,73 @@ class ActiveIngredientsControllerTest < ActionDispatch::IntegrationTest
   test "should not create active ingredient with invalid params" do
     sign_in_as(@admin)
     assert_no_difference "ActiveIngredient.count" do
-      post active_ingredients_path, params: { active_ingredient: {
-        name: "",
-        half_life: nil
-      } }
+      post active_ingredients_path, params: { active_ingredient: { name: "", half_life: nil } }
     end
     assert_response :unprocessable_content
+  end
+
+  # edit
+  test "should redirect edit if not authenticated" do
+    get edit_active_ingredient_path(@active_ingredient)
+    assert_redirected_to new_session_path
+  end
+
+  test "should redirect edit if not admin" do
+    sign_in_as(@user)
+    get edit_active_ingredient_path(@active_ingredient)
+    assert_redirected_to root_path
+  end
+
+  test "should get edit if admin" do
+    sign_in_as(@admin)
+    get edit_active_ingredient_path(@active_ingredient)
+    assert_response :success
+  end
+
+  # update
+  test "should redirect update if not authenticated" do
+    patch active_ingredient_path(@active_ingredient), params: { active_ingredient: { name: "Updated" } }
+    assert_redirected_to new_session_path
+  end
+
+  test "should redirect update if not admin" do
+    sign_in_as(@user)
+    patch active_ingredient_path(@active_ingredient), params: { active_ingredient: { name: "Updated" } }
+    assert_redirected_to root_path
+  end
+
+  test "should update active ingredient if admin" do
+    sign_in_as(@admin)
+    patch active_ingredient_path(@active_ingredient), params: { active_ingredient: { name: "Updated", half_life: 5.0 } }
+    assert_redirected_to active_ingredient_path(@active_ingredient)
+    assert_equal "Updated", @active_ingredient.reload.name
+  end
+
+  test "should not update active ingredient with invalid params" do
+    sign_in_as(@admin)
+    patch active_ingredient_path(@active_ingredient), params: { active_ingredient: { name: "" } }
+    assert_response :unprocessable_content
+  end
+
+  # destroy
+  test "should redirect destroy if not authenticated" do
+    delete active_ingredient_path(@active_ingredient)
+    assert_redirected_to new_session_path
+  end
+
+  test "should redirect destroy if not admin" do
+    sign_in_as(@user)
+    assert_no_difference "ActiveIngredient.count" do
+      delete active_ingredient_path(@active_ingredient)
+    end
+    assert_redirected_to root_path
+  end
+
+  test "should destroy active ingredient if admin" do
+    sign_in_as(@admin)
+    assert_difference "ActiveIngredient.count", -1 do
+      delete active_ingredient_path(@active_ingredient)
+    end
+    assert_redirected_to root_path
   end
 end
