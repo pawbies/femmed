b82aa4fcc71ea43e538a3efd29a08af7c4e6b3c8
pawbies <alex.fuchs@pawbies.net>
2026-03-04 12:25:55 +0100

    advanced search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,23 @@
 class SearchController < ApplicationController
   def index
-    @query = params[:query]
+    @query      = params[:query]
+    @category_id = params[:category_id]
+    @labeler_id  = params[:labeler_id]
+
+    @medications = Medication.all
+    @medications = @medications.where("LOWER(name) LIKE LOWER(?)", "%#{@query.strip}%") if @query.present?
+    @medications = @medications.joins(:categories).where(categories: { id: @category_id }) if @category_id.present?
+    @medications = @medications.where(labeler_id: @labeler_id) if @labeler_id.present?
+    @medications = @medications.order(:name)
 
     if @query.present?
-      @query.strip!
-      @active_ingredients = ActiveIngredient.where("LOWER(name) LIKE LOWER(?)", "%#{@query}%").order(:name)
-      @categories         = Category.where("LOWER(name) LIKE LOWER(?)",         "%#{@query}%").order(:name)
-      @labelers           = Labeler.where("LOWER(name) LIKE LOWER(?)",          "%#{@query}%").order(:name)
-      @medications        = Medication.where("LOWER(name) LIKE LOWER(?)",       "%#{@query}%").order(:name)
+      @active_ingredients = ActiveIngredient.where("LOWER(name) LIKE LOWER(?)", "%#{@query.strip}%").order(:name)
+      @categories         = Category.where("LOWER(name) LIKE LOWER(?)",         "%#{@query.strip}%").order(:name)
+      @labelers           = Labeler.where("LOWER(name) LIKE LOWER(?)",          "%#{@query.strip}%").order(:name)
     else
       @active_ingredients = ActiveIngredient.all
       @categories         = Category.all
       @labelers           = Labeler.all
-      @medications        = Medication.all
     end
   end
 end
