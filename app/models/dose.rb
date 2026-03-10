d066c6493a844726ccbb7be4bc68df614f205943
pawbies <alex.fuchs@pawbies.net>
2026-03-10 11:29:33 +0100

    slight oversight

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -4,6 +4,6 @@ class Dose < ApplicationRecord
   validates :amount_taken, presence: true, numericality: true
   validates :amount_taken,
     comparison: { greater_than: 0, less_than_or_equal_to: ->(record) { record.prescription&.remaining_units } },
-    if: -> { prescription.pack_tracking_enabled? }
+    if: -> { prescription&.pack_tracking_enabled? }
   validates :taken_at, presence: true, comparison: { less_than_or_equal_to: -> { Time.current } }
 end
