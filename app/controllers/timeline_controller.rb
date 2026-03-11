c68d5a507fcd5c18e1db1464797110d8bf1a7f76
pawbies <alex.fuchs@pawbies.net>
2026-03-11 10:09:42 +0100

    optimisations

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -6,8 +6,12 @@ class TimelineController < ApplicationController
       return
     end
 
-    doses = Current.user.doses.where(taken_at: @date.all_day)
+    doses = Current.user.doses.where(taken_at: @date.all_day).includes(:prescription)
     @doses_by_hour = (0..23).index_with { |hour| [] }
     doses.each { |d| @doses_by_hour[d.taken_at.hour] << d }
+
+    diary_entries = Current.user.diary_entries.where(entry_for: @date.all_day)
+    @diary_by_hour = (0..23).index_with { |hour| [] }
+    diary_entries.each { |e| @diary_by_hour[e.entry_for.hour] << e }
   end
 end
