4a62aa1b9a4a76ed442c60bd8ea7c466d2635812
pawbies <alex.fuchs@pawbies.net>
2026-03-16 19:31:01 +0100

    removed password resets

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
   end
   get "profile" => "users#profile"
   resource :session
-  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
