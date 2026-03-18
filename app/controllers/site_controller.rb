3d65f6fae0e8c58722fb7044115a27b24fee7ed2
pawbies <alex.fuchs@pawbies.net>
2026-03-18 17:22:40 +0100

    timeline

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -20,14 +20,21 @@ class SiteController < ApplicationController
     rescue ArgumentError, TypeError
       Date.today
     end
-    if @date.future?
-      redirect_back fallback_location: timeline_path(date: Date.today), notice: "This didnt even happen yet O.O"
-      return
-    end
 
-    doses = Current.user.doses.where(taken_at: @date.all_day).includes(:prescription)
+    doses = Current.user.doses.where(taken_at: @date.all_day).includes(:prescription).to_a
     @doses_by_hour = (0..23).index_with { |hour| [] }
     doses.each { |d| @doses_by_hour[d.taken_at.hour] << d }
+
+    @planned_by_hour = (0..23).index_with { |hour| [] }
+    Current.user.prescriptions.where.not(routine: nil).each do |prescription|
+      prescription_doses = doses.select { |d| d.prescription_id == prescription.id }
+      prescription.routine.occurrences_between(@date.beginning_of_day, @date.end_of_day).each do |time|
+        covered = prescription_doses.any? { |d| (d.taken_at - time).abs <= 1.hour }
+        @planned_by_hour[time.hour] << { prescription: prescription, time: time } unless covered
+      end
+    rescue StandardError
+      next
+    end
   end
 
   def calendar
