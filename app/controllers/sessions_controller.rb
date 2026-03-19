a66836300ac52cbfb02dfda5b4d3e6f286e3a61d
pawbies <alex.fuchs@pawbies.net>
2026-03-19 14:47:03 +0100

    login info

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,13 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
+      if Rails.env.production?
+        PushNotificationService.new(
+          user: user,
+          title: "Heyyo, theres a new login",
+          body: "Someone logged in to your account at #{DateTime.now.utc.strftime("%B %e, %Y %H:%M UTC")}"
+        ).call
+      end
       redirect_to after_authentication_url
     else
       redirect_to new_session_path, alert: "Try another email address or password."
