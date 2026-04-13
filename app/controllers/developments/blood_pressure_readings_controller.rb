6defd6743b24feadbb68c5813badef44e185cab4
pawbies <alex.fuchs@pawbies.net>
2026-04-13 16:10:32 +0200

    edit bpr

--- app/controllers/developments/blood_pressure_readings_controller.rb
+++ app/controllers/developments/blood_pressure_readings_controller.rb
@@ -23,6 +23,17 @@ class Developments::BloodPressureReadingsController < Developments::BaseControll
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @bpr.update blood_pressure_reading_params
+      redirect_to @bpr
+    else
+      render :edit, status: :unprocessable_content
+    end
+  end
+
   private
     def set_blood_pressure_reading
       @bpr = Current.user.blood_pressure_readings.find(params[:id])
