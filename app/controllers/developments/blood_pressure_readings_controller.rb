234fb8fadcd017c896272e085d3d83e63d47c786
pawbies <alex.fuchs@pawbies.net>
2026-04-13 16:05:16 +0200

    new create actions for bpr

--- app/controllers/developments/blood_pressure_readings_controller.rb
+++ app/controllers/developments/blood_pressure_readings_controller.rb
@@ -1,9 +1,23 @@
 class Developments::BloodPressureReadingsController < Developments::BaseController
-  before_action :set_blood_pressure_reading, except: %i[ index ]
+  before_action :set_blood_pressure_reading, except: %i[ index new create ]
 
   def index
     @bpr = Current.user.blood_pressure_readings.order(measured_at: :desc)
     @prescriptions = Current.user.prescriptions
+  end
+
+  def new
+    @bpr = Current.user.blood_pressure_readings.new(measured_at: Time.now)
+  end
+
+  def create
+    @bpr = Current.user.blood_pressure_readings.new(blood_pressure_reading_params)
+
+    if @bpr.save
+      redirect_to @bpr
+    else
+      render :new, status: :unprocessable_content
+    end
   end
 
   def show
@@ -12,5 +26,9 @@ class Developments::BloodPressureReadingsController < Developments::BaseControll
   private
     def set_blood_pressure_reading
       @bpr = Current.user.blood_pressure_readings.find(params[:id])
+    end
+
+    def blood_pressure_reading_params
+      params.expect(blood_pressure_reading: [ :systolic, :diastolic, :bpm, :measured_at ])
     end
 end
