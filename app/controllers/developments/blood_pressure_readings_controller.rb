bd5553498b8e2c14fefff020adaa7f153d421f1f
pawbies <alex.fuchs@pawbies.net>
2026-04-13 14:55:33 +0200

    bp table

--- app/controllers/developments/blood_pressure_readings_controller.rb
+++ app/controllers/developments/blood_pressure_readings_controller.rb
@@ -1,6 +1,6 @@
 class Developments::BloodPressureReadingsController < Developments::BaseController
   def index
-    @pagy, @bpr = pagy(:offset, Current.user.blood_pressure_readings.order(measured_at: :desc))
+    @bpr = Current.user.blood_pressure_readings.order(measured_at: :desc)
     @prescriptions = Current.user.prescriptions
   end
 end
