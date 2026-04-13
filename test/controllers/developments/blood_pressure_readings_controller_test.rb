724eb117d9c2cf47d91b43e7c150c477cfec681e
pawbies <alex.fuchs@pawbies.net>
2026-04-13 18:45:24 +0200

    blood pressure show redesign and tests

--- test/controllers/developments/blood_pressure_readings_controller_test.rb
+++ test/controllers/developments/blood_pressure_readings_controller_test.rb
@@ -1,7 +1,89 @@
 require "test_helper"
 
 class Developments::BloodPressureReadingsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:alice)
+    @other_user = users(:bob)
+    @bpr = blood_pressure_readings(:normal)
+  end
+
+  test "index" do
+    get blood_pressure_readings_url
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    get blood_pressure_readings_url
+    assert_response :success
+  end
+
+  test "show" do
+    get blood_pressure_reading_url(@bpr)
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    get blood_pressure_reading_url(@bpr)
+    assert_response :success
+
+    sign_in_as(@other_user)
+    get blood_pressure_reading_url(@bpr)
+    assert_response :not_found
+  end
+
+  test "new" do
+    get new_blood_pressure_reading_url
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    get new_blood_pressure_reading_url
+    assert_response :success
+  end
+
+  test "create" do
+    post blood_pressure_readings_url, params: { blood_pressure_reading: { systolic: 118, diastolic: 76 } }
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    assert_difference("BloodPressureReading.count") do
+      post blood_pressure_readings_url, params: { blood_pressure_reading: { systolic: 118, diastolic: 76, measured_at: Time.now } }
+    end
+    assert_redirected_to blood_pressure_reading_url(BloodPressureReading.last)
+  end
+
+  test "edit" do
+    get edit_blood_pressure_reading_url(@bpr)
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    get edit_blood_pressure_reading_url(@bpr)
+    assert_response :success
+
+    sign_in_as(@other_user)
+    get edit_blood_pressure_reading_url(@bpr)
+    assert_response :not_found
+  end
+
+  test "update" do
+    patch blood_pressure_reading_url(@bpr), params: { blood_pressure_reading: { systolic: 125 } }
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    patch blood_pressure_reading_url(@bpr), params: { blood_pressure_reading: { systolic: 125 } }
+    assert_redirected_to blood_pressure_reading_url(@bpr)
+    assert_equal 125, @bpr.reload.systolic.to_i
+
+    sign_in_as(@other_user)
+    patch blood_pressure_reading_url(@bpr), params: { blood_pressure_reading: { systolic: 999 } }
+    assert_response :not_found
+  end
+
+  test "destroy" do
+    delete blood_pressure_reading_url(@bpr)
+    assert_redirected_to new_session_url
+
+    sign_in_as(@user)
+    assert_difference("BloodPressureReading.count", -1) do
+      delete blood_pressure_reading_url(@bpr)
+    end
+    assert_redirected_to blood_pressure_readings_url
+  end
 end
